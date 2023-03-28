239a3996e26545072e106c84072930c8a69417ba
Kennedy Musau <kennedy.musauu@gmail.com>
2023-03-28 19:44:22 +0300

    COMPLETED ASSIGNMENT

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+
+const num1 = 1;
+const num2 = 62;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num4 = 14;
+const num3 = 10;
+const mod = num4 % num3;
+
+const max = Math.max(2, 1, 3, 20);
